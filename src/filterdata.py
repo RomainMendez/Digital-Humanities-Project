f4a2563bdb1e03ce700bf643c6fce0584329829b
Romain <Romain.mendez.rm@gmail.com>
2019-02-27 17:19:03 +0100

    formatted filterdata

--- src/filterdata.py
+++ src/filterdata.py
@@ -7,32 +7,32 @@ from bz2 import BZ2File
 
 # a helper function to get the lines from am archive
 def read_jsonlines(bz2_file):
-    text = bz2_file.read().decode('utf-8')
-    for line in text.split('\n'):
-        if line != '':
+    text = bz2_file.read().decode("utf-8")
+    for line in text.split("\n"):
+        if line != "":
             yield line
+
 
 # Parsing the parameters of the program
 filter = sys.argv[2]
 filename = sys.argv[1]
 savefilename = sys.argv[3]
 
-f = BZ2File(os.path.join("data", filename), 'r')
-        
+f = BZ2File(os.path.join("data", filename), "r")
+
 # get the list of articles it contains (= a json object on each line)
 articles = list(read_jsonlines(f))
-        
+
 # Opening the file
-savefile = open(savefilename, 'w')
+savefile = open(savefilename, "w")
 
 # load articles by articles
 for a in articles:
-            
+
     # decode the json string into an object (dict)
     json_article = json.loads(a)
     if filter in json_article["fulltext"]:
 
         savefile.write("**** *" + json_article["date"])
-        savefile.write(
-            json_article["fulltext"]
-        )
+        savefile.write(json_article["fulltext"])
+
